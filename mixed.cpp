72a5368bd92e69f556ec7f292c9c666086369ceb
Matthew Lerma <mlerma232@yahoo.com>
2017-11-08 14:14:21 -0800

    Added files

---
+++ mixed.cpp
@@ -0,0 +1,65 @@
+//#include "mixed.h"
+
+//mixed::mixed()
+//{
+
+//}
+
+//mixed::mixed(int whole)
+//{
+//    value = whole;
+//}
+
+//mixed::mixed(int n, int d)
+//{
+//    value = n/d;
+
+//}
+
+//mixed::mixed(int whole, int n = 0, int d = 1)
+//{
+//    value = whole;
+//}
+
+//mixed::mixed(const fraction &other)
+//{
+//    value = other;
+//}
+
+//mixed& mixed::operator=(int x)
+//{
+//    value = x;
+//    return *this;
+//}
+
+//mixed& mixed::operator=(double d)
+//{
+//    value = d;
+//    return *this;
+//}
+
+
+//mixed& mixed::operator=(const fraction &other)
+//{
+//    value = other;
+//    return *this;
+//}
+
+//mixed& mixed::operator=(const mixed &other)
+//{
+//    if(&other != this)
+//    {
+//        value = other.value;
+//    }
+//    return *this;
+//}
+
+//void mixed::copy(const mixed &other)
+//{
+//    value = other.value;
+//}
+
+//mixed operator+(const mixed &x, const mixed &y)
+//{
+//    return x.value + y.value;
+//}
